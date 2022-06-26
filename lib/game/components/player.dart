0d68be2d831551542478c0d0a46e4b2a59abee34
Soumen <dassoumen2488@gmail.com>
2022-06-26 20:09:15 +0530

    Motions done

--- lib/game/components/player.dart
+++ lib/game/components/player.dart
@@ -1,13 +1,49 @@
+/** Copyright (c) 2021 Razeware LLC
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+    distribute, sublicense, create a derivative work, and/or sell copies of the
+    Software in any work that is designed, intended, or marketed for pedagogical or
+    instructional purposes related to programming, coding, application development,
+    or information technology.  Permission for such use, copying, modification,
+    merger, publication, distribution, sublicensing, creation of derivative works,
+    or sale is expressly withheld.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE. **/
+
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
-import 'package:visiongame/injector/injection.dart';
-
-import '../../base/logger_utils.dart';
 import '../helpers/direction.dart';
+import 'package:flame/sprite.dart';
 
-class Player extends SpriteComponent with HasGameRef {
-  final _logger = locator<LoggerUtils>();
-  final _TAG = "Player";
+class Player extends SpriteAnimationComponent with HasGameRef, CollisionCallbacks {
+  final double _playerSpeed = 300.0;
+  final double _animationSpeed = 0.15;
+
+  late final SpriteAnimation _runDownAnimation;
+  late final SpriteAnimation _runLeftAnimation;
+  late final SpriteAnimation _runUpAnimation;
+  late final SpriteAnimation _runRightAnimation;
+  late final SpriteAnimation _standingAnimation;
+
   Direction direction = Direction.none;
+  Direction _collisionDirection = Direction.none;
+  bool _hasCollided = false;
 
   Player()
       : super(
@@ -17,33 +53,121 @@ class Player extends SpriteComponent with HasGameRef {
   @override
   Future<void> onLoad() async {
     super.onLoad();
-    sprite = await gameRef.loadSprite('player.png');
-    position = gameRef.size / 2;
+    _loadAnimations().then((_) => {animation = _standingAnimation});
   }
-
 
   @override
-  void update(double dt) {
-    super.update(dt);
-    updatePosition(dt);
+  void update(double delta) {
+    super.update(delta);
+    movePlayer(delta);
   }
 
-  updatePosition(double dt) {
+  @override
+  void onCollision(Set<Vector2> intersectionPoints, PositionComponent  other) {
+    super.onCollision(intersectionPoints, other);
+
+  }
+
+  @override
+  void onCollisionEnd(PositionComponent  other) {
+    _hasCollided = false;
+  }
+
+  Future<void> _loadAnimations() async {
+    final spriteSheet = SpriteSheet(
+      image: await gameRef.images.load('player_spritesheet.png'),
+      srcSize: Vector2(29.0, 32.0),
+    );
+
+    _runDownAnimation =
+        spriteSheet.createAnimation(row: 0, stepTime: _animationSpeed, to: 4);
+
+    _runLeftAnimation =
+        spriteSheet.createAnimation(row: 1, stepTime: _animationSpeed, to: 4);
+
+    _runUpAnimation =
+        spriteSheet.createAnimation(row: 2, stepTime: _animationSpeed, to: 4);
+
+    _runRightAnimation =
+        spriteSheet.createAnimation(row: 3, stepTime: _animationSpeed, to: 4);
+
+    _standingAnimation =
+        spriteSheet.createAnimation(row: 0, stepTime: _animationSpeed, to: 1);
+  }
+
+  void movePlayer(double delta) {
     switch (direction) {
       case Direction.up:
-        position.y --;
+        if (canPlayerMoveUp()) {
+          animation = _runUpAnimation;
+          moveUp(delta);
+        }
         break;
       case Direction.down:
-        position.y ++;
+        if (canPlayerMoveDown()) {
+          animation = _runDownAnimation;
+          moveDown(delta);
+        }
         break;
       case Direction.left:
-        position.x --;
+        if (canPlayerMoveLeft()) {
+          animation = _runLeftAnimation;
+          moveLeft(delta);
+        }
         break;
       case Direction.right:
-        position.x ++;
+        if (canPlayerMoveRight()) {
+          animation = _runRightAnimation;
+          moveRight(delta);
+        }
         break;
       case Direction.none:
+        animation = _standingAnimation;
         break;
     }
+  }
+
+  bool canPlayerMoveUp() {
+    if (_hasCollided && _collisionDirection == Direction.up) {
+      return false;
+    }
+    return true;
+  }
+
+  bool canPlayerMoveDown() {
+    if (_hasCollided && _collisionDirection == Direction.down) {
+      return false;
+    }
+    return true;
+  }
+
+  bool canPlayerMoveLeft() {
+    if (_hasCollided && _collisionDirection == Direction.left) {
+      return false;
+    }
+    return true;
+  }
+
+  bool canPlayerMoveRight() {
+    if (_hasCollided && _collisionDirection == Direction.right) {
+      return false;
+    }
+    return true;
+  }
+
+  void moveUp(double delta) {
+    position.add(Vector2(0, delta * -_playerSpeed));
+  }
+
+  void moveDown(double delta) {
+    position.add(Vector2(0, delta * _playerSpeed));
+  }
+
+  void moveLeft(double delta) {
+    position.add(Vector2(delta * -_playerSpeed, 0));
+  }
+
+  void moveRight(double delta) {
+    position.add(Vector2(delta * _playerSpeed, 0));
   }
 }
